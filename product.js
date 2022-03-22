05127c40bb087912877571df8f240622e7456056
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-22 19:27:46 -0400

    created categories, qty in productSchema with comments on what they do

--- product.js
+++ product.js
@@ -23,12 +23,32 @@ const productSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: true,
+    min: 0,
+  },
+  onSale: {
+    type: Boolean,
+    default: false, //this will default to false if we don't declare it in product
+  },
+  categories: [String], //categories will be an array of STRINGS. If input is not a string, it will be made into a string
+  qty: {
+    online: {
+      type: Number,
+      default: 0,
+    },
+    inStore: {
+      type: Number,
+      default: 0, //this will default to 0 if not input is given
+    },
   },
 });
 
 const Product = mongoose.model("Product", productSchema);
 
-const bike = new Product({ name: "Mountain Bike", price: 999 });
+const bike = new Product({
+  name: "Bike Helmet",
+  price: 29.5,
+  categories: ["Cycling", "Safety"],
+});
 bike
   .save()
   .then((data) => {
