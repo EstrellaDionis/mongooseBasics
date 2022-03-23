a074026173759d0b4951db86ff2a72980115a12b
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-23 15:30:23 -0400

    added middleware that is triggered when .save() is called

--- person.js
+++ person.js
@@ -24,14 +24,29 @@ personSchema.virtual("fullName").get(function () {
 });
 
 //if we did want to update the database and be able to change something we would use as set function
-personSchema
-  .virtual("fullname")
-  .get(function () {
-    return this.name.first + " " + this.name.last;
-  })
-  .set(function (v) {
-    this.name.first = v.substr(0, v.indexOf(" ")); //indexOf returns the first index at which a given element can be found in the array or -1 if it is not present
-    this.name.last = v.substr(v.indexOf(" ") + 1); //substr() returns a portion of the string
-  });
+// personSchema
+//   .virtual("fullname")
+//   .get(function () {
+//     return this.name.first + " " + this.name.last;
+//   })
+//   .set(function (v) {
+//     this.name.first = v.substr(0, v.indexOf(" ")); //indexOf returns the first index at which a given element can be found in the array or -1 if it is not present
+//     this.name.last = v.substr(v.indexOf(" ") + 1); //substr() returns a portion of the string
+//   });
+
+//middleware
+//runs whenever we use .save()
+personSchema.pre("save", async function () {
+  //pre execute BEFORE the moethod. ex; .validate .save .remove etc.
+  this.first = "YO";
+  this.last = "MAMA"; //first and last will execute and override the input. Doing this to illustrate that pre is working before post
+  console.log("About to save!!!!");
+});
+
+//runs whenever we use .save()
+personSchema.post("save", async function () {
+  //will execute AFTER the method ex; .validate .save .remove etc.
+  console.log("Just saved!!!!");
+});
 
 const Person = mongoose.model("Person", personSchema);
